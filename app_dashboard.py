fe4e5a4ab6ebc684b25ce6b30308d6cab230311d
Roberto13-Vil <robertvilchis787@gmail.com>
2025-07-21 13:58:49 -0600

    requeriments

--- app_dashboard.py
+++ app_dashboard.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
-import os
 import pickle
 import altair as alt
 import warnings
